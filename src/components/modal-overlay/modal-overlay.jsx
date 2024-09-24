4e76a60802134a79b857d6dc80e04f8be650474f
inna-ux <blinovainna464@gmail.com>
2024-09-24 04:03:59 +0300

    Fix: fixed a bug in previously implemented functionality

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import { func } from "prop-types";
 import style from "./modal-overlay.module.css";
 
-function ModalOverlay  ({ onClick }) {
-  <div onClick={onClick} className={style.modal__overlay}></div>
+
+export const ModalOverlay = ({ onClick }) => {
+  return (
+    <div onClick={onClick} className={style.modal__overlay}></div>
+  )
 };
 
 ModalOverlay.propTypes = {
   onClick: func,
 };
-export default ModalOverlay;
