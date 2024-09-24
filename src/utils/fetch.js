2e409aba759a415dd2d4bd692c5eb814e7b9efa6
inna-ux <blinovainna464@gmail.com>
2024-09-24 04:09:47 +0300

    refactor: change structure of the project

--- src/utils/fetch.js
+++
@@ -1,4 +0,0 @@
-export const jsonFetch = (url) =>
-    fetch(url).then((response) =>
-      response.ok ? response.json() : Promise.reject(response)
-    );
--- src/utils/sum.js
+++
@@ -1,7 +0,0 @@
-export const sum = (arr) => {
-    return arr.reduce((total, current) => {
-      return total + current;
-    }, 0);
-  }
-  
-  
