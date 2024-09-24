cd9f63a3c7a716ea54a5b62a467c21d18c77d12e
inna-ux <blinovainna464@gmail.com>
2024-09-24 03:29:42 +0300

    Fix: fixed a bug in previously implemented functionality

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,84 +1,92 @@
+import React, { useState, useMemo } from "react";
 import {
-    Button,
-    ConstructorElement,
-    CurrencyIcon,
-    DragIcon,
-  } from "@ya.praktikum/react-developer-burger-ui-components";
-  import { useMemo, useState } from "react";
-  import { useGroupIngredients } from '../burger-ingredients/filter-ingredient/usegroupingredient';
-  import { sum } from '../../utils/sum';
-  import  OrderDetails  from '../orderdetails/order-details';
-  import style from './burger-constructor.module.css';
-  import { number, string } from "prop-types";
+  Button,
+  ConstructorElement,
+  CurrencyIcon,
+  DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import OrderDetails from "../orderdetails/order-details";
+import style from './burger-constructor.module.css';
+import {array, number, string } from "prop-types";
 
-  const infoConstructor = (ingredient) => ({
-    price: ingredient?.price ?? 0,
-    thumbnail: ingredient?.image ?? void 0,
-    text: ingredient?.name ?? "",
-  });
-  
 
-export function BurgerConstructor({ chosenElements }) {
-    const [isOpened, setIsOpened] = useState(false);
-    const overallCost = useMemo(
-      () => sum(...chosenElements.map((ingredient) => ingredient.price)),
-      [chosenElements]
-    );
-    const {
-      buns: [burgerBun],
-      mains,
-      sauces,
-    } = useGroupIngredients(chosenElements);
-    const filling = [...mains, ...sauces];
-    const topBottom = {
-      ...infoConstructor(burgerBun),
-      isLocked: true,
-    };
-  
-    return (
-      <section className={`${style.section} mt-25 `}>
-        <ul className={style.list}>
-          <li className={`${style.item__top} ml-8`}>
-            <ConstructorElement type="top" {...topBottom} />
-          </li>
-          <div className={`${style.container__item__filling}`}>
-            <ul className={style.list}>
-              {filling.map((ingredient) => (
-                <li key={ingredient._id} className={style.card__list_item}>
-                  <DragIcon type="primary" />
-                  <ConstructorElement
-                    {...infoConstructor(ingredient)}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-          <li className={`${style.item__bottom}`}>
-            <ConstructorElement type="bottom" {...topBottom} />
-          </li>
-        </ul>
-        <div className={`${style.overallCost__container} mt-10`}>
-          <div className={`${style.overallCost} mr-10`}>
-            <p className="text text_type_digits-medium mr-3">{overallCost}</p>
-            <CurrencyIcon style={{ width: "33px", height: "33px" }} type="primary" />
-          </div>
-          <Button
-            htmlType="button"
-            onClick={() => setIsOpened(true)}
-            type="primary"
-            size="large"
-          >
-            Оформить заказ
-          </Button>
-          {isOpened && <OrderDetails onClose={() => setIsOpened(false)} />}
+
+function BurgerConstructor(props) {
+  const [open, setOpen] = useState(false);
+
+  const buns = useMemo(() => props.data.filter(item => item.type === 'bun'), [props.data]);
+  const saucesMains = useMemo(() => props.data.filter(item => item.type === 'sauce' || item.type === 'main'), [props.data]);
+
+  const totalCost = saucesMains.reduce((prev, item) => {
+    return prev + item.price
+  }, 0);
+
+
+
+  return (
+    <section className={`${style.section} pt-25 pl-10 pb-10 `}>
+      <ul className={`${style.list_main} pl-4 pr-4  `}>
+        <li className={`${style.item__top} ml-8 pr-4 mb-4`}>
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text={`${buns[0].name} (верх)`}
+            price={buns[0].price}
+            thumbnail={buns[0].image}
+
+          />
+        </li>
+
+        <div className={`${style.container__item__filling} pr-2`}>
+          <ul className={style.list}>
+            {saucesMains.map((item, index) => (
+              <li key={index.toString()} className={style.card__list_item}>
+                <DragIcon type="primary" />
+                <ConstructorElement
+                  text={item.name}
+                  price={item.price}
+                  thumbnail={item.image}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
-      </section>
-    );
-  }
-  
-  BurgerConstructor.propTypes = {
-    price: number,
-    thumbnail: string,
-    text: string,
-    overallCost: number,
-  };
+
+        <li className={`${style.item__bottom} ml-8 pr-4 mt-4`}>
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text={`${buns[0].name} (низ)`}
+            price={buns[0].price}
+            thumbnail={buns[0].image}
+          />
+        </li>
+      </ul>
+      <div className={`${style.full_price_container} mt-10`}>
+        <div className={`${style.full_price} mr-10`}>
+          <p className="text text_type_digits-medium mr-3">{totalCost}</p>
+          <CurrencyIcon style={{ width: "33px", height: "33px" }} type="primary" />
+        </div>
+        <Button
+          htmlType="button"
+          type="primary"
+          size="large"
+          onClick={() => setOpen(true)}
+        >
+          Оформить заказ
+        </Button>
+        <OrderDetails open={open} onClose={() => setOpen(false)} />
+      </div>
+    </section>
+  );
+}
+
+BurgerConstructor.propTypes = {
+  price: number,
+  thumbnail: string,
+  text: string,
+  totalCostt: number,
+  data: array
+};
+
+export default BurgerConstructor;
