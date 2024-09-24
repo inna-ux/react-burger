755b5d7fa2393dbafabf0d2df6d0027ceeec433d
inna-ux <blinovainna464@gmail.com>
2024-09-24 03:21:39 +0300

    fix: fixed a bug in previously implemented functionality

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,28 +1,56 @@
 import React from 'react';
+import axios from "axios";
 import AppHeader from "../app-header/app-header.jsx";
-import BurgerIngredients from '../burger-ingredients/burger-ingredients.jsx'
+import BurgerIngredients from '../burger-ingredients/burger-ingredients.jsx';
+import BurgerConstructor from '../burger-constructor/burger-constructor.jsx';
 import appStyles from './app.module.css';
-import { useFetch } from  '../../usefetch/usefetch.js'
+import { useState, useEffect } from 'react';
 
+const Ingredient = 'https://norma.nomoreparties.space/api/ingredients';
 
-const apiUrlIngredients = 'https://norma.nomoreparties.space/api/ingredients';
 function App() {
-  const { data } = useFetch(apiUrlIngredients);
-  const ingredients = data?.data;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const response = await axios.get(Ingredient);
+        setData(response.data.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
 
 
-  
+
   return (
 
     <div className={appStyles.app}>
 
       <AppHeader />
 
-      <main className={appStyles.main} >
+      {!loading &&
+        !error &&
+        data.length > 0 && (<main className={appStyles.main} >
+          <BurgerIngredients data={data} />
+          <BurgerConstructor data={data} />
+        </main>)}
 
-        
-            <BurgerIngredients  ingredients={ingredients} />         
-      </main>
+      {loading && 'Загрузка...'}
+
+      {error && (
+        <div>
+          <h2>Сообщение об ошибке:</h2>
+          <p>{error.message}</p>
+        </div>
+      )}
 
     </div>
 
@@ -32,4 +60,3 @@ function App() {
 }
 
 export default App;
-
