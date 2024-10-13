4f3b5b11db1006755936f18be836c9165ea8c110
inna-ux <blinovainna464@gmail.com>
2024-10-13 12:43:17 +0300

    feat: получение данных о актуальном номере заказа

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,15 +1,17 @@
-import { bool, func, node } from "prop-types";
+import { useSelector } from "react-redux";
 import done from "../../images/done.svg";
-import { Modal } from "../modal/modal";
+
 import style from "./order-details.module.css";
 
-function OrderDetails({ onClose, children, open }) {
+function OrderDetails() {
+  const orderNumber = useSelector((state) => state.createdOrder.orderNumber);
+
   return (
-    <Modal children={children} open={open} onClose={onClose}>
+    <>
       <p
         className={`${style.order__number} text text_type_digits-large  mb-8 ml-25 mr-25`}
       >
-        034536
+        {orderNumber}
       </p>
       <p className="text text_type_main-medium mb-15 ">идентификатор заказа</p>
       <img
@@ -23,14 +25,8 @@ function OrderDetails({ onClose, children, open }) {
       <p className="text text_type_main-default text_color_inactive mb-30">
         Дождитесь готовности на орбитальной станции
       </p>
-    </Modal>
+    </>
   );
 }
-
-OrderDetails.propTypes = {
-  onClose: func,
-  open: bool,
-  children: node,
-};
 
 export default OrderDetails;
