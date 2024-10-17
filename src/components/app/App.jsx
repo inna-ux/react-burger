a8e8e16003e0694a7e42cc8aeb3d3a886ba7f047
inna-ux <blinovainna464@gmail.com>
2024-10-17 11:34:40 +0300

    Feat: маршрут для страниц:  /login - страница авторизации, /register - страница регистрации, /forgot-password - страница восстановления пароля, /reset-password - страница сброса пароля

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,10 +4,14 @@ import { useDispatch } from "react-redux";
 import AppHeader from "../app-header/app-header.jsx";
 import appStyles from "./app.module.css";
 import { getIngredientsData } from "../../services/actions/ingredients-data.js";
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import HomePage from "../../pages/home/home.jsx";
 import IngredientsDetails from "../ingredient-details/ingredient-details.jsx";
 import { Modal } from "../modal/modal.jsx";
+import Login from "../../pages/login/login.jsx";
+import ForgotPassword from "../../pages/forgot-password/forgot-password.jsx";
+import ResetPassword from "../../pages/reset-password/reset-password.jsx";
+import Register from "../../pages/register/register.jsx";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,9 +23,8 @@ function App() {
     navigate(-1);
   };
 
-
   const background = location.state && location.state.background;
- 
+
   useEffect(() => {
     dispatch(getIngredientsData());
   }, [dispatch]);
@@ -30,25 +33,26 @@ function App() {
     <div className={appStyles.app}>
       <AppHeader />
       <Routes location={background || location}>
-      <Route path="/" element={<HomePage />} />
-      <Route path='/ingredients/:id'
-               element={<IngredientsDetails />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/ingredients/:id" element={<IngredientsDetails />} />
       </Routes>
 
       {background && (
         <Routes>
-	        <Route
-	          path='/ingredients/:id'
-	          element={
-	            <Modal title={"Детали ингредиента"} onClose={handleModalClose}>
-	              <IngredientsDetails />
-	            </Modal>
-	          }
-	        />
+          <Route
+            path="/ingredients/:id"
+            element={
+              <Modal title={"Детали ингредиента"} onClose={handleModalClose}>
+                <IngredientsDetails />
+              </Modal>
+            }
+          />
         </Routes>
       )}
-
-      
     </div>
   );
 }
