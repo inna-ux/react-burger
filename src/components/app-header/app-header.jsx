b2ffc9cdcefde68ada2d7b998ce4d57219a2fd77
inna-ux <blinovainna464@gmail.com>
2024-10-29 03:55:00 +0300

    Feat: сделала в главном меню пункты активными и неактивными в зависимости от текущего URL

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -6,41 +6,64 @@ import {
   ListIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import headerStyles from "./app-header.module.css";
+import { NavLink, useLocation } from "react-router-dom";
 
 function AppHeader() {
+  const location = useLocation();
   return (
     <header className={`${headerStyles.header} pt-4 pb-4`}>
       <nav className={headerStyles.nav}>
         <ul className={`${headerStyles.nav__list} `}>
           <li className={headerStyles.nav__item}>
-            <a
-              href="/"
-              className={`${headerStyles.nav__link} ${headerStyles.white} pl-5 pr-5 pb-4 pt-4  text text_type_main-default `}
+            <NavLink
+              to="/"
+              style={({ isActive }) => {
+                return {
+                  color: isActive ? "white" : "#8585ad",
+                };
+              }}
+              className={`${headerStyles.nav__link}  pl-5 pr-5 pb-4 pt-4  text text_type_main-default  `}
             >
-              <BurgerIcon type="primary" />
+              <BurgerIcon
+                type={location.pathname === "/" ? "primary" : "secondary"}
+              />
               Конструктор
-            </a>
+            </NavLink>
           </li>
           <li className={`${headerStyles.nav_item}`}>
-            <a
-              href="*"
+            <NavLink
+              to="*"
+              style={({ isActive }) => {
+                return {
+                  color: isActive ? "white" : "#8585ad",
+                };
+              }}
               className={`${headerStyles.nav__link} pl-5 pr-5 pb-4 pt-4 ml-2 text text_type_main-default text_color_inactive`}
             >
-              <ListIcon type="secondary" />
+              <ListIcon
+                type={location.pathname === "/*" ? "primary" : "secondary"}
+              />
               Лента заказов
-            </a>
+            </NavLink>
           </li>
         </ul>
         <div className={headerStyles.logo}>
           <Logo />
         </div>
-        <a
-          href="/login"
+        <NavLink
+          to="/profile"
+          style={({ isActive }) => {
+            return {
+              color: isActive ? "white" : "#8585ad",
+            };
+          }}
           className={`${headerStyles.nav__link} pl-5 pr-5 pb-4 pt-4 text text_type_main-default text_color_inactive`}
         >
-          <ProfileIcon type="secondary" />
+          <ProfileIcon
+            type={location.pathname === "/profile" ? "primary" : "secondary"}
+          />
           Личный кабинет
-        </a>
+        </NavLink>
       </nav>
     </header>
   );
