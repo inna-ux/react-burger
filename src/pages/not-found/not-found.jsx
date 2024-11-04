b7952d478c156cc3950bbdf07f431cf5be18990c
inna-ux <blinovainna464@gmail.com>
2024-11-05 01:52:36 +0300

    Feat: добавила страницу 404

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import styles from './not-found.module.css';
-
-
+import styles from "./not-found.module.css";
 
 export function NotFound404() {
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        
         <div className={styles.content}>
           <h1>Oops! 404 Error</h1>
-          <p>The page you requested does not exist</p>
+          <p>Запрошенная вами страница не существует</p>
           <br />
           <br />
-          <p>check the address or try <Link to='/' className={styles.link}>homepage</Link></p>
+          <p>
+            проверьте адрес или перейдите на{" "}
+            <Link to="/" className={styles.link}>
+              Главную
+            </Link>
+          </p>
         </div>
       </div>
     </div>
