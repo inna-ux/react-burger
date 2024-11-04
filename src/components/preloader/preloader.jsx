764467911d00994fd9a2e96ddc9171d0fdc2cc67
inna-ux <blinovainna464@gmail.com>
2024-11-05 00:48:11 +0300

    Feat: сверстала и добавила прелоудер

---
+++ src/components/preloader/preloader.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./preloader.module.css";
+
+export const Preloader = () => {
+  return <div className={styles.loader}></div>;
+};
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,15 +1,17 @@
-
 import { useSelector } from "react-redux";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import BurgerIngredients from "../../components/burger-ingredients/burger-ingredients.jsx";
 import BurgerConstructor from "../../components/burger-constructor/burger-constructor.jsx";
 import styles from "./home.module.css";
+import { Preloader } from "../../components/preloader/preloader.jsx";
 
 function HomePage() {
   const { loading, error, data } = useSelector(
     (store) => store.listIngredients
   );
+
+  const { orderRequest } = useSelector((store) => store.createdOrder);
 
   return (
     <>
@@ -27,7 +29,8 @@ function HomePage() {
           <p>{error}</p>
         </div>
       )}
-      {loading && "Загрузка..."}
+      {loading && <Preloader />}
+      {orderRequest && <Preloader />}
     </>
   );
 }
