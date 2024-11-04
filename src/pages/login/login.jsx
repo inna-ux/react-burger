0420a52b153da8ac2c3d6d8632e9ced44729c736
inna-ux <blinovainna464@gmail.com>
2024-11-05 01:40:37 +0300

    Feat: Добавила в Login и Register отправку нужных экшенов в обработчик события submit

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   EmailInput,
   Button,
@@ -6,47 +6,55 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import { useNavigate } from "react-router-dom";
 import styles from "./login.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { loginAction } from "../../services/actions/user/set-user";
 
 function Login() {
-  const [value, setValue] = React.useState("bob@example.com");
-
-  const [pasvalue, setPasValue] = React.useState("password");
+  const [signIn, setSignIn] = useState({
+    email: "",
+    password: "",
+  });
+  const loginFailed = useSelector((state) => state.user.loginFailed);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const onChange = (e) => {
-    setValue(e.target.value);
+    setSignIn({ ...signIn, [e.target.name]: e.target.value });
   };
-
-  const onChangePas = (e) => {
-    setPasValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginAction(signIn.email, signIn.password));
   };
   const registerClick = () => {
     navigate("/register");
-
-  }
+  };
   const fogotPasswordClick = () => {
-    navigate("/forgot-password")
-  }
+    navigate("/forgot-password");
+  };
+
   return (
     <div className={styles.container}>
-      <h2 className={`mb-6 text text_type_main-medium`}>
-        Вход
-      </h2>
+      <h2 className={`mb-6 text text_type_main-medium`}>Вход</h2>
 
-      <form className={styles.form}>
+      <form onSubmit={onSubmit} className={styles.form}>
         <EmailInput
           onChange={onChange}
-          value={value}
+          value={signIn.email}
           name={"email"}
           isIcon={false}
         />
         <PasswordInput
           placeholder="Пароль"
-          onChange={onChangePas}
-          value={pasvalue}
+          onChange={onChange}
+          value={signIn.password}
           name={"password"}
           extraClass="mb-2 mt-6 mb-6"
         />
+        {loginFailed && (
+          <p className={`${styles.error} text text_type_main-default mb-2`}>
+            Неверный логин или пароль
+          </p>
+        )}
 
         <Button htmlType="submit" type="primary" size="medium">
           Войти
@@ -57,22 +65,30 @@ function Login() {
         <p className="text text_type_main-default text_color_inactive">
           Вы — новый пользователь?
         </p>
-        
-          <Button htmlType="button" onClick={registerClick} type="secondary" size="medium">
-            Зарегистрироваться
-          </Button>
-       
+
+        <Button
+          htmlType="button"
+          onClick={registerClick}
+          type="secondary"
+          size="medium"
+        >
+          Зарегистрироваться
+        </Button>
       </div>
 
       <div className={styles.block}>
         <p className="text text_type_main-default text_color_inactive">
           Забыли пароль?
         </p>
-        
-          <Button htmlType="button" onClick={fogotPasswordClick} type="secondary" size="medium">
-            Восстановить пароль
-          </Button>
-        
+
+        <Button
+          htmlType="button"
+          onClick={fogotPasswordClick}
+          type="secondary"
+          size="medium"
+        >
+          Восстановить пароль
+        </Button>
       </div>
     </div>
   );
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   EmailInput,
   Input,
@@ -7,56 +7,55 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import { useNavigate } from "react-router-dom";
 import styles from "./register.module.css";
+import { useDispatch } from "react-redux";
+import userAction from "../../services/actions/user/create-user";
 
 function Register() {
-  const [value, setValue] = React.useState("value");
-  const inputRef = React.useRef(null);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    password: "",
+    email: "",
+  });
   const navigate = useNavigate();
-
-  const [Emailvalue, setEmailValue] = React.useState("bob@example.com");
+  const dispatch = useDispatch();
   const onChange = (e) => {
-    setEmailValue(e.target.value);
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   };
-
-  const [Pasvalue, setPasValue] = React.useState("password");
-  const onPasChange = (e) => {
-    setPasValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(userAction(userInfo.email, userInfo.password, userInfo.name));
   };
 
   const loginClick = () => {
-    navigate("/login")
-
-  }
+    navigate("/login");
+  };
 
   return (
     <div className={styles.container}>
-      <h2 className={`text text_type_main-medium mb-6`}>
-        Регистрация
-      </h2>
-      <form className={styles.form}>
+      <h2 className={`text text_type_main-medium mb-6`}>Регистрация</h2>
+      <form onSubmit={onSubmit} className={styles.form}>
         <Input
           type={"text"}
           placeholder={"Имя"}
-          onChange={(e) => setValue(e.target.value)}
-          value={value}
+          onChange={onChange}
+          value={userInfo.name}
           name={"name"}
           error={false}
           errorText={"Ошибка"}
           size={"default"}
           extraClass="ml-1"
-          ref={inputRef}
         />
         <EmailInput
           placeholder="E-mail"
           onChange={onChange}
-          value={Emailvalue}
+          value={userInfo.email}
           name={"email"}
           extraClass="mb-2"
         />
         <PasswordInput
           placeholder="Пароль"
-          onChange={onPasChange}
-          value={Pasvalue}
+          onChange={onChange}
+          value={userInfo.password}
           name={"password"}
           extraClass="mb-2"
         />
@@ -68,11 +67,15 @@ function Register() {
         <p className="text text_type_main-default text_color_inactive">
           Уже зарегистрированы?
         </p>
-        
-          <Button htmlType="button" onClick={loginClick} type="secondary" size="small">
-            Войти
-          </Button>
-        
+
+        <Button
+          htmlType="button"
+          onClick={loginClick}
+          type="secondary"
+          size="small"
+        >
+          Войти
+        </Button>
       </div>
     </div>
   );
