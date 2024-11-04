492156b423a9368ba709f40463c1d8370b12b9ca
inna-ux <blinovainna464@gmail.com>
2024-11-05 00:58:44 +0300

    Fix: убрала экшены: для обновления токенов, экшен для получения данных пользователя, экшен для восстановления пароля и изменения

--- src/services/actions/user/update-user.js
+++ src/services/actions/user/update-user.js
@@ -1,41 +1,34 @@
 import { refreshToken, updateUser } from "../../../utils/api";
-import { REFRESH_TOKEN_FAILED, REFRESH_TOKEN_SUCCESS } from "./refresh-token";
 import { setCookie } from "../../../utils/cooke";
 
-export const UPDATE_USER_REQUEST = 'UPDATE_USER_REQUEST';
-export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS';
-export const UPDATE_USER_FAILED = 'UPDATE_USER_FAILED';
+export const UPDATE_USER_REQUEST = "UPDATE_USER_REQUEST";
+export const UPDATE_USER_SUCCESS = "UPDATE_USER_SUCCESS";
+export const UPDATE_USER_FAILED = "UPDATE_USER_FAILED";
 
 const updateUserAction = (name, email, password) => (dispatch) => {
   dispatch({
-    type: UPDATE_USER_REQUEST
+    type: UPDATE_USER_REQUEST,
   });
   updateUser(name, email, password)
-    .then(res => { dispatch({ type: UPDATE_USER_SUCCESS, payload: res.user }) })
-    .catch(() => {
-      const token = localStorage.getItem('refreshToken');
-      if (token) {
-        refreshToken()
-          .then(res => {
-            if (res && res.success) {
-              setCookie('accessToken', res.accessToken, { expires: 1200 });
-              localStorage.setItem('refreshToken', res.refreshToken);
-              dispatch({ type: REFRESH_TOKEN_SUCCESS })
-            }
-            else {
-              dispatch({ type: REFRESH_TOKEN_FAILED })
-            }
-          })
-          .then(() => {
-            updateUser(name, email, password)
-              .then((res) => {
-                dispatch({ type: UPDATE_USER_SUCCESS, payload: res.user })
-              })
-          })
-      } else { dispatch({ type: UPDATE_USER_FAILED }) }
+    .then((res) => {
+      dispatch({ type: UPDATE_USER_SUCCESS, payload: res.user });
     })
-}
+    .catch(() => {
+      const token = localStorage.getItem("refreshToken");
+      if (token) {
+        refreshToken().then((res) => {
+          if (res && res.success) {
+            setCookie("accessToken", res.accessToken, { expires: 1200 });
+            localStorage.setItem("refreshToken", res.refreshToken);
+            updateUser(name, email, password).then((res) => {
+              dispatch({ type: UPDATE_USER_SUCCESS, payload: res.user });
+            });
+          }
+        });
+      } else {
+        dispatch({ type: UPDATE_USER_FAILED });
+      }
+    });
+};
 
 export default updateUserAction;
-
-
