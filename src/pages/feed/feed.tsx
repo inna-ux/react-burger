794cacc323ce57706488589622fd30e802c54c37
inna-ux <blinovainna464@gmail.com>
2024-11-28 19:47:38 +0300

    feat: реализовала вторую часть страницы Feed, Fix: перенесла часть кода функции подключения и отключения соединение WebSocket в родительский компонент Feed

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,7 +1,21 @@
 import styles from './feed.module.css'
 import OrderFeed from './order-feed/order-feed';
+import { useDispatch } from '../../utils/types/hook';
+import { URL_WSS } from '../../utils/api';
+import { wsConnectionClosed, wsConnectionStart } from '../../services/actions/websocket';
+import { useEffect } from 'react';
+import InfoOrders from './info-orders/info-orders';
+
+
 
 function Feed() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(wsConnectionStart(`${URL_WSS}/orders/all`));
+    return () => {
+      dispatch(wsConnectionClosed())
+    };
+  }, [dispatch]);
   
   
     return (
@@ -10,7 +24,7 @@ function Feed() {
           <main className={styles.main}>
             
               <OrderFeed  />
-              {/* <InfoOrders /> */}
+              <InfoOrders />
             
           </main>
        
--- src/pages/feed/order-feed/order-feed.tsx
+++ src/pages/feed/order-feed/order-feed.tsx
@@ -1,28 +1,21 @@
 import styles from './order-feed.module.css';
 import GroupOrderCard from '../../../components/group-order-card/group-order-card';
-import { useDispatch, useSelector } from '../../../utils/types/hook';
-import { URL_WSS } from '../../../utils/api';
-import { wsConnectionClosed, wsConnectionStart } from '../../../services/actions/websocket';
-import { useEffect } from 'react';
+import {  useSelector } from '../../../utils/types/hook';
+
 import { Preloader } from '../../../components/preloader/preloader';
 
 
 function OrderFeed (): React.JSX.Element {
-  const dispatch = useDispatch();
+
   const ordersData = useSelector(state => state.wsFeedOrders.orders);
 
-  useEffect(() => {
-    dispatch(wsConnectionStart(`${URL_WSS}/orders/all`));
-    return () => {
-      dispatch(wsConnectionClosed())
-    };
-  }, [dispatch]);
+ 
     return (
         <section className={`${styles.section}`}>
              <h1 className={`${styles.title} text text_type_main-large mt-10 mb-5`}>
        Лента заказов
       </h1>
-      <div className={`${styles.ingredients__block}`}>
+      <div className={`${styles.ingredients__block} mr-2`}>
         <GroupOrderCard ordersData={ ordersData} />
 
       </div>
